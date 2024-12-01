55cae0a749407dc0cbd13eec9c80c4319ebfe464
Mohamed Nasr <mnasr0868@gmail.com>
2024-12-01 20:41:49 +0200

    conditional styling the styled components and make sure that the prop used for styling purposes is starting with '$' to not make a clash with othet props

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,6 +1,32 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
+const ControlsContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 0.5rem;
+margin-bottom: 1.5rem;
+`
+const Label = styled.label`
+display: block;
+margin-bottom: 0.5rem;
+font-size: 0.75rem;
+font-weight: 700;
+letter-spacing: 0.1em;
+text-transform: uppercase;
+color: ${({ $invalid }) => $invalid ? '#f87171' : '#6b7280'} ;
+`
+const Input = styled.input`
+width: 100%;
+padding: 0.75rem 1rem;
+line-height: 1.5;
+background-color:${({$invalid})=> $invalid ? '#fed2d2' : '#d1d5db'} ;
+color:${({$invalid})=> $invalid ? '#ef4444' : '#374151'} ;
+border: 1px solid ${({$invalid})=> $invalid ? '#f73f3f' : 'transparent'};
+border-radius: 0.25rem;
+box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -21,39 +47,14 @@ export default function AuthInputs() {
   const emailNotValid = submitted && !enteredEmail.includes('@');
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
-  const ControlsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    margin-bottom: 1.5rem;
-  `
-  const Label = styled.label`
-    display: block;
-    margin-bottom: 0.5rem;
-    font-size: 0.75rem;
-    font-weight: 700;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;
-    color: #6b7280;
-  `
-  const Input = styled.input`
-    width: 100%;
-    padding: 0.75rem 1rem;
-    line-height: 1.5;
-    background-color: #d1d5db;
-    color: #374151;
-    border: 1px solid transparent;
-    border-radius: 0.25rem;
-    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  `
   return (
     <div id="auth-inputs">
       <ControlsContainer>
         <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            $invalid={emailNotValid}
             // style={{ 
             //   color : emailNotValid ? 'red' : '#374151'
             //  }}
@@ -61,10 +62,10 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</Label>
+          <Label $invalid={emailNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
